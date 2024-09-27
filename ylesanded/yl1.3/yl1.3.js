ee8b9c7007e256ab8596f4dc1faaf1aa4c7240b1
robinristo <robin-risto.kivit@voco.ee>
2024-09-27 12:30:27 +0300

    muutsin yl1.3.js osa ridadest kommentiteks.

--- ylesanded/yl1.3/yl1.3.js
+++ ylesanded/yl1.3/yl1.3.js
@@ -1,3 +1,4 @@
-const aste = prompt("Sisestage astme alus: ");
-const astendaja = prompt("Sisestage astendaja: ");
+//const aste = prompt("Sisestage astme alus: ");
+//const astendaja = prompt("Sisestage astendaja: ");
+//Ülemised ei tööta, peab mingi node.js lahendust kasutama vist?
 console.log(Math.pow(aste, astendaja));
