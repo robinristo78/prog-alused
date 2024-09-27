7498fe7aab68f6854849a70e13f3aa83d584c5e4
robinristo <robin-risto.kivit@voco.ee>
2024-09-27 13:48:25 +0300

    yl1.3.js kood on nüüd korrektselt kirjutatud.

--- ylesanded/yl1/yl1.3.js
+++ ylesanded/yl1/yl1.3.js
@@ -1,8 +1,19 @@
-//let aste = prompt("Sisestage astme alus: ");
-//let astendaja = prompt("Sisestage astendaja: ");
-//Ülemised ei tööta, peab mingi node.js lahendust kasutama
-let aste = 2;
-let astendaja = 4;
-let tulemus = aste ** astendaja;
-//console.log(Math.pow(aste, astendaja));
-console.log(tulemus);
+//let aste = 2;
+//let astendaja = 4;
+
+const readline = require('node:readline');
+
+const rl = readline.createInterface(
+   {
+    input: process.stdin,
+    output: process.stdout,
+   } 
+);
+
+rl.question("Sisestage aste: ", aste => { 
+    rl.question("Sisestage astendaja: ", astendaja => { 
+        let result = aste ** astendaja;
+        console.log(result);
+        rl.close()
+    });
+});
