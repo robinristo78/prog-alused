519bc7bbd2a0b188916377cc8e7800db7994c396
robinristo <robin-risto.kivit@voco.ee>
2024-09-27 13:27:54 +0300

    Liigutasin ylesande failid ümber.

--- ylesanded/yl1.3/yl1.3.js
+++
@@ -1,4 +0,0 @@
-//const aste = prompt("Sisestage astme alus: ");
-//const astendaja = prompt("Sisestage astendaja: ");
-//Ülemised ei tööta, peab mingi node.js lahendust kasutama vist?
-console.log(Math.pow(aste, astendaja));
---
+++ ylesanded/yl1/yl1.3.js
@@ -0,0 +1,8 @@
+//let aste = prompt("Sisestage astme alus: ");
+//let astendaja = prompt("Sisestage astendaja: ");
+//Ülemised ei tööta, peab mingi node.js lahendust kasutama
+let aste = 2;
+let astendaja = 4;
+let tulemus = aste ** astendaja;
+//console.log(Math.pow(aste, astendaja));
+console.log(tulemus);
