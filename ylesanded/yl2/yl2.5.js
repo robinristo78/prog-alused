9b8f5078c034cf92f8f760b8f9f6a3ab0e8cd74d
robinristo <robin-risto.kivit@voco.ee>
2024-10-02 13:03:07 +0300

    Lisatud Package.json & utils/input.js

--- ylesanded/yl2/yl2.5.js
+++ ylesanded/yl2/yl2.5.js
@@ -1,12 +1,24 @@
-const readline = require('node:readline');
+// const readline = require('node:readline');
 
-const rl = readline.createInterface(
-   {
-    input: process.stdin,
-    output: process.stdout,
-   } 
-);
+// const rl = readline.createInterface(
+//    {
+//     input: process.stdin,
+//     output: process.stdout,
+//    } 
+// );
+
+import rl from "../utils/input.js";
 
 rl.question('Sisestage kirja suurus megabaitides: ', suurus =>{
-    
-} );
+    //console.log("töötab");
+    //Kontrolli kas suurus on number
+    if (typeof suurus === "number") {
+        rl.question('Sisestage kirja teema pealkirja (vajutage enter, et jätta tühjaks): ', pealkiri =>{
+
+        });
+    } else {
+        console.log("ERROR: Peab sisestama numbri!");
+        rl.close();
+    }
+
+});
