a6130e71be506b031f173ea08bd7121c0dc6016e
robinristo <robin-risto.kivit@voco.ee>
2024-10-02 13:22:21 +0300

    salvestus: yl2.5.js

--- ylesanded/yl2/yl2.5.js
+++ ylesanded/yl2/yl2.5.js
@@ -10,11 +10,33 @@
 import rl from "../utils/input.js";
 
 rl.question('Sisestage kirja suurus megabaitides: ', suurus =>{
-    //console.log("töötab");
     //Kontrolli kas suurus on number
-    if (typeof suurus === "number") {
-        rl.question('Sisestage kirja teema pealkirja (vajutage enter, et jätta tühjaks): ', pealkiri =>{
+    if (!isNaN(suurus)) {
+        //Muuda suurus ujukomaarvuks
+        suurus = parseFloat(suurus);
+        rl.question('Sisestage kirja teema pealkirja (vajutage enter, et jätta tühjaks): ', pealkiri => {
+            rl.question('Kas kirjaga on kaasas fail? (y/n): ', manus => { 
+                manus = manus.toLowerCase();
+                if (manus !== 'y' && manus !== 'n') {
+                    console.log("ERROR: Peab sisestama 'y' või 'n'!");
+                    rl.close();
+                    process.exit();
+                }
 
+                // kirjal ei ole teema pealkirja,
+                // kiri sisaldab manusena faili 
+                // ja kirja suurus ületab 1 MB.
+                
+                let filter1 = (pealkiri == "") ? true : false;
+                let filter2 = false;
+                let filter3 = false;
+                
+                // if (){
+
+                // } 
+
+                rl.close();
+            }); 
         });
     } else {
         console.log("ERROR: Peab sisestama numbri!");
