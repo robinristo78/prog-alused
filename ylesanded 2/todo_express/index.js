b173a4d6739b384d464dc23cabc73537b79d8bac
robinristo <robin-risto.kivit@voco.ee>
2024-10-29 14:21:29 +0200

    index.ejs + views

---
+++ ylesanded 2/todo_express/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', "ejs");
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    // res.send(path.join(__dirname, "views"));
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.send('test changes');
+});
+
+app.listen(3001, () => {
+    console.log('Server started at http://localhost:3001');
+});
