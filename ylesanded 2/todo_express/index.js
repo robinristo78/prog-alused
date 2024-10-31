d397b88ff0a5bb55c1a00159bb4a1399e02c930c
robinristo <robin-risto.kivit@voco.ee>
2024-10-31 13:34:21 +0200

    Write data to file

--- ylesanded 2/todo_express/index.js
+++ ylesanded 2/todo_express/index.js
@@ -4,32 +4,67 @@ const fs = require('node:fs');
 
 const app = express();
 
+const readFile = (filename) =>{
+    return new Promise((resolve, reject) => { 
+        fs.readFile('./tasks', 'utf-8', (error, data) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+
+            // console.log('in fs.readFile(./tasks)');
+            const tasks = data.split('\n');
+            resolve(tasks);
+        });
+    });
+}
+
+
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    // res.send(path.join(__dirname, "views"));
-
-    // const tasks = ['Study JS', 'Study templating', 'Study HTTP'];  
     
+    // old:
+    // fs.readFile('./tasks', 'utf-8', (error, data) => {
+    //     if (error) {
+    //         console.error(error);
+    //         return;
+    //     }
 
-    fs.readFile('./tasks', 'utf-8', (error, data) => {
-        if (error) {
-            console.error(error);
-            return;
-        }
+    //     // console.log('in fs.readFile(./tasks)');
+    //     const tasks = data.split('\n'); 
+    //     res.render('index', {tasks: tasks});
+    // });
 
-        // console.log('in fs.readFile(./tasks)');
-        const tasks = data.split('\n'); 
+    // console.log('in app.get(/)');
+
+    readFile('./tasks').then(tasks => {
+        console.log(tasks);
         res.render('index', {tasks: tasks});
     });
 
-    
-
-    // console.log('in app.get(/)');
 });
 
+app.use(express.urlencoded( { extended: true} ));
 
+app.post('/', (req, res) => {
+    console.log('form sent data');
+    console.log(req.body.task);
+
+    readFile('./tasks').then(tasks => {
+        tasks.push(req.body.task);
+        const data = tasks.join('\n');
+        console.log(data);
+        fs.writeFile('./tasks', data, error => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            res.redirect('/');
+        });
+    });
+});
 
 // app.get('/about', (req, res) => {
 //     res.send('test changes');
