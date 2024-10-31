9779fb84ed2d272bd11635a9da3ee46a7431bc9f
robinristo <robin-risto.kivit@voco.ee>
2024-10-31 12:29:20 +0200

    Read data from file

--- ylesanded 2/todo_express/index.js
+++ ylesanded 2/todo_express/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('node:fs');
 
 const app = express();
 
@@ -8,14 +9,33 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     // res.send(path.join(__dirname, "views"));
-    const tasks = ['Study JS', 'Study templating', 'Study HTTP'];  
-    res.render('index', {tasks: tasks});
+
+    // const tasks = ['Study JS', 'Study templating', 'Study HTTP'];  
+    
+
+    fs.readFile('./tasks', 'utf-8', (error, data) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+
+        // console.log('in fs.readFile(./tasks)');
+        const tasks = data.split('\n'); 
+        res.render('index', {tasks: tasks});
+    });
+
+    
+
+    // console.log('in app.get(/)');
 });
+
+
 
 // app.get('/about', (req, res) => {
 //     res.send('test changes');
 // });
 
+console.log('in app');
 app.listen(3001, () => {
     console.log('Server started at http://localhost:3001');
 });
