20645cc48f35f8c83770914861d58dfc36411c0b
robinristo <robin-risto.kivit@voco.ee>
2024-11-06 13:51:59 +0200

    Add error handling

--- ylesanded 2/todo_express/index.js
+++ ylesanded 2/todo_express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('node:fs');
+const { error } = require('console');
 
 const app = express();
 
@@ -34,45 +35,56 @@ app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-
+    // tasks list data from file
     readFile('./tasks.json').then(tasks => {
         // console.log(tasks);
-        res.render('index', {tasks: tasks});
+        res.render('index', {
+            tasks: tasks,
+            error: null
+        });
     });
-
 });
 
 app.use(express.urlencoded( { extended: true} ));
 
 app.post('/', (req, res) => {
-    console.log('form sent data');
-    console.log(req.body.task);
+    // console.log('form sent data');
+    // console.log(req.body.task);
+    let error = null;
+    if (req.body.task.trim().length == 0) {
+        error = 'Please insert correct task data';
+        readFile('./tasks.json').then(tasks => {
+            res.render('index', {
+                tasks: tasks,
+                error: error
+            });
+        });
+    } else { 
+        readFile('./tasks.json').then(tasks => {
+            // tasks.push(req.body.task);
+            // const data = tasks.join('\n');
+            // console.log(data);
 
-    readFile('./tasks.json').then(tasks => {
-        // tasks.push(req.body.task);
-        // const data = tasks.join('\n');
-        // console.log(data);
+            let index;
+            if (tasks.length === 0) {
+                index = 0;
+            } else {
+                index = tasks[tasks.length-1].id + 1;
+            }
 
-        let index;
-        if (tasks.length === 0) {
-            index = 0;
-        } else {
-            index = tasks[tasks.length-1].id + 1;
-        }
+            const newTask = {
+                "id": index,
+                "task": req.body.task
+            }
+            
+            tasks.push(newTask);
 
-        const newTask = {
-            "id": index,
-            "task": req.body.task
-        }
-        
-        tasks.push(newTask);
+            data = JSON.stringify(tasks, null, 2);
 
-        data = JSON.stringify(tasks, null, 2);
-
-        writeFile('./tasks.json', data);
-    });
-    // tuleb väljapool readFile-i teha redirect muidu leht ei lae õigesti.
-    res.redirect('/');
+            writeFile('./tasks.json', data);
+            res.redirect('/');
+        });
+    }
 });
 
 // app.get('/about', (req, res) => {
