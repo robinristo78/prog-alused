c20d8e82c02e35f78694fe506ddc22f2b833466c
robinristo <robin-risto.kivit@voco.ee>
2024-11-29 12:27:35 +0200

    Ül3: Create project template files

--- ylesanded 3/weather_express/index.js
+++ ylesanded 3/weather_express/index.js
@@ -5,6 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+
 app.get('/', (req, res) => {
     res.render('index');
 });
