e77c22327cd5559448f341cbba78a625739cd9d5
robinristo <robin-risto.kivit@voco.ee>
2024-11-29 13:35:23 +0200

    Ül3: Create form request responce with API data

--- ylesanded 3/weather_express/index.js
+++ ylesanded 3/weather_express/index.js
@@ -8,6 +8,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 const key = '86688134c6cbfc0eac1d2522e5772aea'; //API Key
 let city = 'Tartu';
 
@@ -29,7 +32,28 @@ app.get('/', (req, res) => {
                 city: city,
                 temp: temp
         });
+    });
+});
 
+app.post('/', (req, res) => {
+    // console.log('form sent data');
+    // console.log(req.body);
+    // res.redirect('/');
+
+    let city = req.body.cityname;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let description = data.weather[0].description;
+            let city = data.name;
+            let temp = Math.round(parseFloat(data.main.temp)-273.15);
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+        });
     });
 });
 
