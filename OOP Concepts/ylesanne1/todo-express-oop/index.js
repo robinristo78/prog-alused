d547dbdf7e668fdaf4be1252f8ed725f84a92d43
robinristo <robin-risto.kivit@voco.ee>
2024-12-04 13:56:13 +0200

    OOP Ül1: pooleli

--- OOP Concepts/ylesanne1/todo-express-oop/index.js
+++ OOP Concepts/ylesanne1/todo-express-oop/index.js
@@ -1,14 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import todoRoutes from './routes/todos.js';
+
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/json-test', (req, res) => {
-    res.send({
-        message: 'json test ok'
-    });
-});
+// app.get('/json-test', (req, res) => {
+//     res.send({
+//         message: 'json test ok'
+//     });
+// });
+
+app.use('/todos', todoRoutes);
 
 app.listen(3003, () => {
     console.log('server is connected at port 3003: https://localhost:3003/');
