205e1a67b55140d19d4a8967c9a5d526f680338b
robinristo <robin-risto.kivit@voco.ee>
2024-12-10 09:00:14 +0200

    OOP Ül1: Get all Todos

--- OOP Concepts/ylesanne1/todo-express-oop/controllers/todos.js
+++ OOP Concepts/ylesanne1/todo-express-oop/controllers/todos.js
@@ -18,6 +18,10 @@ class todoController {
             newTask: newTodo
         });
     }
+
+    getTodos(req, res){
+        res.json({tasks: this.TODOS});
+    } 
 }
 
 export const TodoController = new todoController();
