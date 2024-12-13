781e486c6d146dd48ab2fd7da8f00bc1faa07aeb
robinristo <robin-risto.kivit@voco.ee>
2024-12-13 10:37:15 +0200

    OOP Ül2: Update the Todo by id

--- OOP Concepts/ylesanne2/todo-ts-oop/src/controllers/todos.ts
+++ OOP Concepts/ylesanne2/todo-ts-oop/src/controllers/todos.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { Todo } from '../models/todo';
+import exp from 'constants';
 
 const todos: Todo[] =  [];
 
@@ -23,6 +24,27 @@ export const getTodos = (req: Request, res: Response, next: NextFunction) => {
     try {
         res.status(201).json({
             tasks: todos
+        });
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export const updateTodo = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const todoId = req.params.id;
+        const updatedTask = (req.body as {task: string}).task;
+        const todoIndex = todos.findIndex(todo => todo.id === todoId);
+
+        if (todoIndex < 0) {
+            throw new Error('Could not find todo with such id');
+        }
+
+        todos[todoIndex] = new Todo(todos[todoIndex].id, updatedTask);
+
+        res.status(201).json({
+            message: 'Todo is updated!',
+            updatedTask: todos[todoIndex] 
         });
     } catch(error) {
         console.log(error);
