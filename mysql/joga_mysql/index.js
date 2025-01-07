98f36d9df22f03e3d2ca1b5c36909d3788a0a933
robinristo <robin-risto.kivit@voco.ee>
2025-01-07 14:33:46 +0200

    As a user I can see all yoga articles. See #2

--- mysql/joga_mysql/index.js
+++ mysql/joga_mysql/index.js
@@ -1,5 +1,8 @@
 const app = require('./utils/app');
 const db = require('./utils/db');
+const articleRoutes = require('./routes/articles')
+
+app.use('/', articleRoutes);
 
 app.listen(3006, () => {
     console.log('Web server is connected');
