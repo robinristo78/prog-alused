4ad1900eada7ab290802fdf1b1c9e95bed65a55f
robinristo <robin-risto.kivit@voco.ee>
2025-01-07 14:58:39 +0200

    As a user I can see the single yoga article. See #4

--- mysql/joga_mysql/controllers/articles.js
+++ mysql/joga_mysql/controllers/articles.js
@@ -9,6 +9,16 @@ const getAllArticles = (req, res) => {
     });
 }
 
+const getArticleBySlug = (req, res) => {
+    let sql = `SELECT * FROM article WHERE slug="${req.params.slug}"`;
+    db.query(sql, (error, result) => {
+        res.render('article', {
+            article: result
+        });
+    });
+}
+
 module.exports = {
-    getAllArticles
+    getAllArticles,
+    getArticleBySlug
 } 
