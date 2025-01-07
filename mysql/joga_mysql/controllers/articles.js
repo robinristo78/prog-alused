a4bfe19ab01038d0ec3afa589853d1b3d40b742a
robinristo <robin-risto.kivit@voco.ee>
2025-01-07 15:19:27 +0200

    Add author name into single article

--- mysql/joga_mysql/controllers/articles.js
+++ mysql/joga_mysql/controllers/articles.js
@@ -12,8 +12,18 @@ const getAllArticles = (req, res) => {
 const getArticleBySlug = (req, res) => {
     let sql = `SELECT * FROM article WHERE slug="${req.params.slug}"`;
     db.query(sql, (error, result) => {
-        res.render('article', {
-            article: result
+        // res.render('article', {
+        //     article: result
+        // });
+        const article = result[0];
+        let author_id = result[0].author_id;
+        let sql =  `SELECT * FROM author WHERE id="${author_id}"`;
+        db.query(sql, (error, result) => {
+            const author = result[0];
+            article['author_name'] = author.name;
+            res.render('article', {
+                article: article
+            }); 
         });
     });
 }
