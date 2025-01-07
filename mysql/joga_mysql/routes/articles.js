cd367d024e74b19d7bf98d77623eac028138471d
robinristo <robin-risto.kivit@voco.ee>
2025-01-07 14:57:16 +0200

    Display and article by slug

--- mysql/joga_mysql/routes/articles.js
+++ mysql/joga_mysql/routes/articles.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const articleController = require('../controllers/articles');
 
 router.get('/', articleController.getAllArticles);
+router.get('/article/:slug', articleController.getArticleBySlug);
 
 module.exports = router;
